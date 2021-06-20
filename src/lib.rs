dbd1c06a3406836e62776a4591c24fc20c678af7
Jaiden Bernard <thisjaiden@gmail.com>
2021-06-19 19:43:10 -0700

    doc improvements, error rewrite, and strings

--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,24 @@
 //! - Provide a generalized format for sharing and using Minecraft's data types
 //! - Simplify the decoding and encoding of network data
 //!
-//! # Examples
-//! Reading a VarInt from bytes:
-//! ```rust
-//! fn main() -> Result<()> {
-//!     // This should be a valid varint (TODO)
-//!     let sample_data = [0x00, 0x24];
-//!     let decoded_value = golden_apple::VarInt::from_bytes(&sample_data)?;
-//!     assert_eq!(decoded_value.0, golden_apple::VarInt::from_value(24));
-//!     Ok(())
-//! }
-//! ```
-//!
+//! # Usage
+//! Proprietary Minecraft types like `VarInt`, `VarLong`, and `Position` are a part of the top
+//! level crate. Types that can be fully represented in Rust have encoders/decoders under
+//! `golden_apple::generalized`, in case it isn't striaghtforward to do so.
+
 #[derive(Debug)]
+/// Represents an error that can occur while using one of the libraries functions.
 pub enum Error {
+    /// The datastream representing a VarInt exceded the maximum acceptable size.
     VarIntTooLong,
-    VarIntMissingData,
-    VarIntReaderError,
-    VarIntWriterError
+    /// An error occured while using a `Read` type to parse.
+    ReaderError(std::io::Error),
+    /// An error occured while using a `Write` type to parse.
+    WriterError(std::io::Error),
+    /// There was not enough data present to parse.
+    MissingData,
+    /// A boolean had a value other than true or false.
+    InvalidBool
 }
 
 impl std::fmt::Display for Error {
@@ -40,7 +40,7 @@ impl From<Error> for () {
     }
 }
 
-/// Represents a Java Int using between 1-5 bytes.
+/// Represents a Java Int (i32) using between 1-5 bytes.
 #[derive(Eq, Clone, Copy, Debug)]
 pub struct VarInt {
     value: i32,
@@ -71,7 +71,7 @@ impl VarInt {
     }
     /// Creates a VarInt from a series of bytes. Returns the value and the amount of bytes used if
     /// creation is successful.
-    pub fn from_bytes(data: &[u8]) -> Result<(VarInt, u8), Error> {
+    pub fn from_bytes(data: &[u8]) -> Result<(VarInt, usize), Error> {
         let mut iterator = data.iter();
         let mut result = 0;
 
@@ -85,7 +85,7 @@ impl VarInt {
                     read = val;
                 }
                 None => {
-                    return Err(Error::VarIntMissingData);
+                    return Err(Error::MissingData);
                 }
             }
 
@@ -97,7 +97,7 @@ impl VarInt {
             }
 
             if (read & msb) == 0 {
-                return Ok((VarInt {value: result, length: i}, i));
+                return Ok((VarInt {value: result, length: i}, i as usize));
             }
         }
         // This will never occur.
@@ -111,22 +111,16 @@ impl VarInt {
         let mask: u8 = !msb;
     
         for i in 0..5 {
-            let mut read: [u8; 1] = [0x00];
-            match reader.read_exact(&mut read) {
-                Ok(_) => {},
-                Err(_) => {
-                    return Err(Error::VarIntReaderError);
-                }
-            }
+            let read = read_byte(reader)?;
     
-            result |= ((read[0] & mask) as i32) << (7 * i);
+            result |= ((read & mask) as i32) << (7 * i);
     
             // The 5th byte is only allowed to have the 4 smallest bits set
-            if i == 4 && (read[0] & 0xf0 != 0) {
+            if i == 4 && (read & 0xf0 != 0) {
                 return Err(Error::VarIntTooLong);
             }
     
-            if (read[0] & msb) == 0 {
+            if (read & msb) == 0 {
                 return Ok(VarInt {value: result, length: i});
             }
         }
@@ -147,15 +141,15 @@ impl VarInt {
             if val != 0 {
                 match writer.write_all(&[tmp | msb]) {
                     Ok(_) => {},
-                    Err(_) => {
-                        return Err(Error::VarIntWriterError);
+                    Err(e) => {
+                        return Err(Error::WriterError(e));
                     }
                 }
             } else {
                 match writer.write_all(&[tmp]) {
                     Ok(_) => {},
-                    Err(_) => {
-                        return Err(Error::VarIntWriterError);
+                    Err(e) => {
+                        return Err(Error::WriterError(e));
                     }
                 }
                 return Ok(());
@@ -198,18 +192,78 @@ impl VarInt {
     }
 }
 
-pub struct String {
 
+pub mod generalized {
+    use super::Error;
+    use super::read_byte;
+
+    /// Reads a `String` from a type implimenting `Read`. This function returns the string without the
+    /// VarInt length prefix, and does not verify that the text is utf8.
+    pub fn string_from_reader<R: std::io::Read>(reader: &mut R) -> Result<String, Error> {
+        let string_len = super::VarInt::from_reader(reader)?.value();
+        let mut text: Vec<u8> = vec![0; string_len as usize];
+        match reader.read_exact(&mut text) {
+            Ok(_) => {},
+            Err(e) => {
+                return Err(Error::ReaderError(e));
+            }
+        }
+        unsafe {
+            // Minecraft is known to put weird stuff in their strings, so we're not going to double check.
+            return Ok(String::from_utf8_unchecked(text));
+        }
+    }
+    /// Reads a `String` from a series of bytes. This function returns the string without the VarInt
+    /// length prefix, but does include the size of that VarInt in the final size calculation. The text
+    /// is not verified to be utf8.
+    pub fn string_from_bytes(bytes: &[u8]) -> Result<(String, usize), Error> {
+        let string_len = super::VarInt::from_bytes(bytes)?;
+        let mut text: Vec<u8> = vec![0; string_len.0.value() as usize];
+        let finbytes = bytes.split_at(string_len.1).1;
+        for i in 0..text.len() {
+            text[i] = finbytes[i];
+        }
+        unsafe {
+            // Minecraft is known to put weird stuff in their strings, so we're not going to double check.
+            return Ok((String::from_utf8_unchecked(text), string_len.0.value() as usize + string_len.1));
+        }
+    }
+    /// Woefully unnessicary. Seriously, bools are just 0x00 or 0x01.
+    pub fn bool_from_reader<R: std::io::Read>(reader: &mut R) -> Result<bool, Error> {
+        let byte = read_byte(reader)?;
+        if byte == 0x00 {
+            return Ok(false);
+        }
+        if byte == 0x01 {
+            return Ok(true);
+        }
+        return Err(Error::InvalidBool);
+    }
+    /// Woefully unnessicary. Seriously, bools are just 0x00 or 0x01.
+    /// Side note: this function will always read just a single byte, making half of the
+    /// return type pointless.
+    pub fn bool_from_bytes(bytes: &[u8]) -> Result<(bool, usize), Error> {
+        if bytes.len() < 1 {
+            return Err(Error::MissingData);
+        }
+        if bytes[0] == 0x00 {
+            return Ok((false, 1));
+        }
+        if bytes[0] == 0x01 {
+            return Ok((true, 1));
+        }
+        return Err(Error::InvalidBool);
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn varint_values() {
-        // construct known values:
-        // shortest possible
-        // longest possible
-        // mid value
-        // negative
+fn read_byte<R: std::io::Read>(reader: &mut R) -> Result<u8, Error> {
+    let mut read: [u8; 1] = [0x00];
+    match reader.read_exact(&mut read) {
+        Ok(_) => {
+            return Ok(read[0]);
+        },
+        Err(e) => {
+            return Err(Error::ReaderError(e));
+        }
     }
 }
